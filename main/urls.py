41373dfbb0ffd761f1a6fec95f36c7a6d76d095d
5480021 <essi.pohjalainen@gmail.com>
2025-08-14 15:24:05 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    #path('login/', views.login_view, name='login'),
     path('secret/', views.secret_page, name='secret'),
     path('upload/', views.upload_file, name='upload'),
     path('debug-demo/', views.debug_demo, name='debug_demo'),
